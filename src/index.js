d2d1951a8952dfad74fa2997df65ed4d63a88e12
MishynMykhailo <misham18092001@gmail.com>
2022-12-25 12:40:06 +0200

    update name project

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/testtask/">
+      <BrowserRouter basename="/phonebook-testtask/">
         <App />
       </BrowserRouter>
     </Provider>
